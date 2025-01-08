c76dccad89e1917bf7c2cea61efdd536ed854fcb
MK <99416359+MK316@users.noreply.github.com>
2025-01-08 16:18:49 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.write("〽️ Graduate course: Phonology and English Education")
-st.write("- Semester: Spring 2025")
+st.write("Semester: Spring 2025")
 st.caption("Topic: Acoustics and perception studies")
 st.caption("🕑 Last updated: Jan.08, 2025")
