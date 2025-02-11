0f81051e55f0b774a44ae82fcd8c662b300013c3
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 22:46:57 +0900

    Update ➕Chapter01.py

--- pages/➕Chapter01.py
+++ pages/➕Chapter01.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["Lecture slides", "Apps", "Download"])
+tab1, tab2, tab3 = st.tabs(["📖 Lecture slides", "🌀 Apps", "💾 Download"])
 
 
 with tab1:
