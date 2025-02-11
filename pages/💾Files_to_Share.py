7450b255f8a87a89b566d2b09256d7659494d115
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 22:10:21 +0900

    Update 💾Files_to_Share.py

--- pages/💾Files_to_Share.py
+++ pages/💾Files_to_Share.py
@@ -5,15 +5,15 @@ def main():
 
     tabs = st.tabs(["Google drive files", "Others"])
 
-with tabs[0]:
-    st.markdown("#### Access to Shared Google Drive: Restricted list")
-    st.markdown("[Google drive](https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link): Group members, Google Drive")
-
-    # Button to open GitHub ID page
-    if st.button("Open Google drive"):
-        st.markdown(f'<meta http-equiv="refresh" content="0;url=https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link">', unsafe_allow_html=True)
-
-with tabs[1]:
+    with tabs[0]:
+        st.markdown("#### Access to Shared Google Drive: Restricted list")
+        st.markdown("[Google drive](https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link): Group members, Google Drive")
+    
+        # Button to open GitHub ID page
+        if st.button("Open Google drive"):
+            st.markdown(f'<meta http-equiv="refresh" content="0;url=https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link">', unsafe_allow_html=True)
+    
+    with tabs[1]:
 
 
 if __name__ == "__main__":
