a4a08cb919b9e3af27637be91ff51b4757ee8fe6
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 23:35:48 +0900

    Update IV._Praat.py

--- pages/IV._Praat.py
+++ pages/IV._Praat.py
@@ -6,6 +6,7 @@ with tab1:
     st.image("https://github.com/MK316/Acoustics/raw/main/images/praat-icon.jpg", caption="Doing Phonetics by Computer")
     st.markdown("---")
     st.markdown("""
+    - Praat is a free software for speech analysis and phonetic research, widely used in linguistics and language education. It offers spectrograms, pitch tracking, formant analysis, and speech synthesis, with scripting support for automation.
     - [Software to download]()
     - How to install: Just double click the icon appearing after downloading file.
     """)
