91c763805556a6d9d45cf4d1e07c2e08e38c8731
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 23:00:02 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,5 +5,5 @@ st.markdown("""
   - Semester: Spring 2025
   - Topic: Acoustics and Perception Experiment
   """)
-st.image("https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo1.jpg", "What is sound? How we perceive sounds differently?")
+st.image("https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo1.jpg", "What is sound? How learners perceive English sounds similarly or differently?")
 st.caption("🕑 Last updated: Feb.11, 2025")
