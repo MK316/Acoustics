4938747271de643899ac6eeed07655cc80c87b64
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 23:06:20 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,8 +8,6 @@ st.markdown("""
 
 st.image(
     "https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo2.jpg",
-    caption="What is sound, and how does it relate to spoken English? How do English learners perceive sounds differently based on their linguistic background?",
-    use_container_width=True  # Automatically resizes
-)
+    caption="What is sound, and how does it relate to spoken English? How do English learners perceive sounds differently based on their linguistic background?")
 
 st.caption("🕑 Last updated: Feb.11, 2025")
