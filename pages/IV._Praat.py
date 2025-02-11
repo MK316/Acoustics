ce5e7ed661bdb05f42d916536cacf1897fe61d47
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 23:33:22 +0900

    Update IV._Praat.py

--- pages/IV._Praat.py
+++ pages/IV._Praat.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.image("https://github.com/MK316/Acoustics/raw/main/images/praat-icon.jpg", caption="Doing Phonetics by Computer")
+tab1, tab2 = st.tabs(["Praat software", "manual"])
+
+with tab1:
+  st.image("https://github.com/MK316/Acoustics/raw/main/images/praat-icon.jpg", caption="Doing Phonetics by Computer")
+  st.markdown("---")
+  st.markdown("""
+  - [Software to download]()
+  - How to install: Just double click the icon appearing after downloading file.
+  """)
+
+with tab2:
