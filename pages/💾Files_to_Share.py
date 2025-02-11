7ec3842c35de5a10d5c3a28db202139718375d88
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 22:15:29 +0900

    Update 💾Files_to_Share.py

--- pages/💾Files_to_Share.py
+++ pages/💾Files_to_Share.py
@@ -5,12 +5,12 @@ import streamlit as st
 tab1, tab2 = st.tabs(["Google drive files", "Others"])
 
 with tab1:
-    st.markdown("#### Access to Shared Google Drive: Restricted list")
-    st.markdown("[Google drive](https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link): Group members, Google Drive")
+    st.markdown("#### Access to Padlet")
+    st.markdown("[Padlet](https://padlet.com/mirankim316/acoustics): Group members, Google Drive")
     
     # Button to open GitHub ID page
-    if st.button("Open Google drive"):
-       st.markdown(f'<meta http-equiv="refresh" content="0;url=https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link">', unsafe_allow_html=True)
+    if st.button("Open Padlet"):
+       st.markdown(f'<meta http-equiv="refresh" content="0;url=https://padlet.com/mirankim316/acoustics">', unsafe_allow_html=True)
     
 with tab2:
     st.markdown("#### Other Resources")
