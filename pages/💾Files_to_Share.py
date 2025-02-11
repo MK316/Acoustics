98b3461163cc3c24ef215995eb38d2706c163896
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 22:09:33 +0900

    Update 💾Files_to_Share.py

--- pages/💾Files_to_Share.py
+++ pages/💾Files_to_Share.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-with tabs[3]:
+def main():
+#    st.title('Coding4ET Tutorials')
+
+    tabs = st.tabs(["Google drive files", "Others"]
+
+with tabs[0]:
     st.markdown("#### Access to Shared Google Drive: Restricted list")
     st.markdown("[Google drive](https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link): Group members, Google Drive")
 
@@ -8,3 +13,8 @@ with tabs[3]:
     if st.button("Open Google drive"):
         st.markdown(f'<meta http-equiv="refresh" content="0;url=https://drive.google.com/drive/folders/1f0T4-BfKIwwowVB6JMLsmDMw0w0DQyvi?usp=drive_link">', unsafe_allow_html=True)
 
+with tabs[1]:
+
+
+if __name__ == "__main__":
+    main()
