8c11f4812fd15f9f2c4408a440f64d64c9da0bcc
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 23:03:08 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
 st.markdown("""
-  #### 🔸 Phonology & English Education")
+  #### 🔸 Phonology & English Education
   - Semester: Spring 2025
   - Topic: Acoustics and Perception Experiment
   """)
-st.image("https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo1.jpg", "What is sound, and how does it relate to spoken English? How do English learners perceive sounds differently based on their linguistic background?")
+
+st.image(
+    "https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo1.jpg",
+    caption="What is sound, and how does it relate to spoken English? How do English learners perceive sounds differently based on their linguistic background?",
+    use_container_width=True  # Automatically resizes
+)
+
 st.caption("🕑 Last updated: Feb.11, 2025")
