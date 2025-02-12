d0601622e68d7f3a7801db3f6b610cdb2f5a13f1
MK <99416359+MK316@users.noreply.github.com>
2025-02-12 20:24:33 +0900

    Update ➕Chapter02.py

--- pages/➕Chapter02.py
+++ pages/➕Chapter02.py
@@ -128,7 +128,7 @@ with tab2:
 with tab3:
     st.title('Guitar String Harmonics Simulator')
     # mode = st.sidebar.slider('Select Mode', 0, 2, 0, key='harmonic_mode')  # Ensuring slider is associated with this tab
-    plot_harmonics(mode)
+    plot_harmonics()
 
 
 
