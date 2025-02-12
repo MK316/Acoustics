50a6a25e02064b3a3c1be643d2db9be83308d22f
MK <99416359+MK316@users.noreply.github.com>
2025-02-12 19:13:46 +0900

    Update ➕Chapter02.py

--- pages/➕Chapter02.py
+++ pages/➕Chapter02.py
@@ -37,7 +37,7 @@ with tab2:
     
     st.markdown(
         f"""
-        <iframe width="700" height="400" src="{video_url}" 
+        <iframe width="400" height="300" src="{video_url}" 
         title="YouTube video player" frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
