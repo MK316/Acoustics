11fc8039ac370bdaab0dcc53ab45ad5f4d16a8b8
MK <99416359+MK316@users.noreply.github.com>
2025-02-13 15:30:54 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ st.image(
     width=250  # Adjust width as needed
 )
 
-st.caption("What is sound, and how does it relate to spoken English? ")
+st.caption("What is sound, and how does it relate to teaching/learning spoken English? ")
 st.caption("How do English learners perceive sounds differently based on their linguistic background?")
 st.markdown("---")
 st.caption("🕑 Last updated: Feb.11, 2025")
