41d7ad0e8e58a5032402608c5827bb078292c735
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 20:49:57 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,10 +9,8 @@ st.markdown("""
 st.image(
     "https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo4.png",
     width=600,  # Adjust width as needed
-    caption= "Physical aspect of sound"
+    caption= "Physical aspect of sound; What is sound, and how does it relate to teaching/learning spoken English?; How do English learners perceive sounds differently based on their linguistic background?"
 )
 
-st.caption("What is sound, and how does it relate to teaching/learning spoken English? ")
-st.caption("How do English learners perceive sounds differently based on their linguistic background?")
 st.markdown("---")
 st.caption("🕑 Last updated: Feb.11, 2025")
