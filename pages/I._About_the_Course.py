4ab7172a1ccb9b83d2b95c9a6f1e9144d88aa79e
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 20:57:24 +0900

    Update I._About_the_Course.py

--- pages/I._About_the_Course.py
+++ pages/I._About_the_Course.py
@@ -25,7 +25,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Create tabs for different sections of the course
-tabs = st.tabs(["🍐 Course Overview", "Materials", "🍏 Evaluation", "🍒 Assignments", "🍋 Links", "📆 Calendar"])
+tabs = st.tabs(["🍐 Course Overview", "💙 Materials", "🍏 Evaluation", "🍒 Assignments", "🍋 Links", "📆 Calendar"])
 
 # Course Overview tab
 with tabs[0]:
