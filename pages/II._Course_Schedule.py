af50fb8648089cba95ae7990b3d7b577765d65e4
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 21:32:21 +0900

    Update II._Course_Schedule.py

--- pages/II._Course_Schedule.py
+++ pages/II._Course_Schedule.py
@@ -26,6 +26,6 @@ with tabs[1]:
 with tabs[2]:
     st.header("🐾 Files to share: on Padlet")
     st.write("This Padlet serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
-    st.components.v1.iframe("https://padlet.com/mirankim316/acoustics", width=800, height=600)
+    st.components.v1.iframe("https://padlet.com/mirankim316/acoustics", width=700, height=800)
 
 
