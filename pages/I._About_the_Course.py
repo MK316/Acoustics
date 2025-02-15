480d78c03751d42980f6497a77020ea07f4efee2
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 21:21:10 +0900

    Update I._About_the_Course.py

--- pages/I._About_the_Course.py
+++ pages/I._About_the_Course.py
@@ -87,7 +87,7 @@ with tabs[3]:
 
 # Content for the Links tab
 with tabs[4]:
-    st.header("Links")
+    st.header("Files to share: on Padlet")
     st.write("Useful links will be updated here. This could include additional reading materials, online resources, and related external websites.")
     st.components.v1.iframe("https://padlet.com/mirankim316/acoustics", width=800, height=600)
 
