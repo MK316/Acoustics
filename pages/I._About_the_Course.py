187e9b9697e33258d1df06572715b2eeb671c413
MK <99416359+MK316@users.noreply.github.com>
2025-02-18 18:13:24 +0900

    Update I._About_the_Course.py

--- pages/I._About_the_Course.py
+++ pages/I._About_the_Course.py
@@ -69,12 +69,15 @@ with tabs[0]:
     st.markdown("""
     <div class="justify-text">
     This course is tailored for English educators, introducing essential skills for integrating acoustics into language teaching. We will demystify the fundamentals of speech sounds and auditory perception, emphasizing their practical application in English pronunciation and listening comprehension.
+    
     Students will:
+    
     - Explore the fundamentals of speech sounds in the context of English language learning.
     - Develop a working knowledge of experimental design in acoustics and perception studies.
     - Learn basic Praat coding skills for implementing and analyzing acoustic experiments.
     - Create experimental tools for testing pronunciation and listening skills using speech data
     - _Future Coursework:_ While this course empowers you with the tools to design and initiate experiments, a subsequent course will focus on conducting detailed analyses and applying these findings to enhance teaching practices effectively. This future course will help solidify your ability to integrate research into your language teaching.
+    
     </div>
     """, unsafe_allow_html=True)
     st.markdown("---")
