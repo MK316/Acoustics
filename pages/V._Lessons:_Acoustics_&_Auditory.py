e69ed779e89518da804c0155372d6c63da9c06bf
MK <99416359+MK316@users.noreply.github.com>
2025-02-19 17:47:04 +0900

    Update V._Lessons:_Acoustics_&_Auditory.py

--- pages/V._Lessons:_Acoustics_&_Auditory.py
+++ pages/V._Lessons:_Acoustics_&_Auditory.py
@@ -14,17 +14,20 @@ st.markdown("""
 <li>1.3.3 Aperiodic waves</li>
 </ul>
 1.4 Acoustic Filters... 19    
+""", unsafe_allow_html=True)
+st.markdown("---")
 
-
-
+st.markdown("""
 ##### Chapter 2. The Acoustic Theory of Speech Production: Deriving Schwa... 25
 2.1 Voicing... 25    
 2.2 Voicing Quanta... 28    
 2.3 Vocal Tract Filtering... 30     
 2.4 Pendulums, Standing Waves, and Vowel Formants... 32    
 2.5 Discovering Nodes and Antinodes in an Acoustic Tube... 45    
+""", unsafe_allow_html=True)
+st.markdown("---")
 
-
+st.markdown("""
 ##### Chapter 3. Digital Signal Processing... 49  
 3.1 Continuous versus Discrete Signals...  49    
 3.2 Analog-to-Digital Conversion... 50    
@@ -41,16 +44,20 @@ st.markdown("""
 <li>3.3.5 Linear predictive coding (LPC) ... 71 </li> 
 <li>3.3.6 Spectra and spectrograms ... 80 </li> 
 </ul>
+""", unsafe_allow_html=True)
+st.markdown("---")
 
-
+st.markdown("""
 ##### Chapter 4. Basic Audition ... 82    
 4.1 Anatomy of the Peripheral Auditory System ... 82  
 4.2 The Auditory Sensation of Loudness ... 83  
 4.3 Frequency Response of the Auditory System ... 88  
 4.4 Saturation and Masking ... 90  
 4.5 Auditory Representations ... 93  
+""", unsafe_allow_html=True)
+st.markdown("---")
 
-
+st.markdown("""
 ##### Chapter 5. Speech Perception ... 100  
 5.1 Auditory Ability Shapes Speech Perception ... 101  
 5.2 Phonetic Knowledge Shapes Speech Perception ... 104  
