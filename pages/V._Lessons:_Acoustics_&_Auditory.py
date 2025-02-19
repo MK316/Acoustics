f6ec1739afad129ae3ed4d16e0b87d60b93c99a2
MK <99416359+MK316@users.noreply.github.com>
2025-02-19 17:32:00 +0900

    Update V._Lessons:_Acoustics_&_Auditory.py

--- pages/V._Lessons:_Acoustics_&_Auditory.py
+++ pages/V._Lessons:_Acoustics_&_Auditory.py
@@ -13,9 +13,41 @@ st.markdown("""
 1.3.3 Aperiodic waves  
 1.4 Acoustic Filters  
 
-#### Chapter 2.
-#### Chapter 
-#### Chapter 
-#### Chapter 
+#### Chapter 2. The Acoustic Theory of Speech Production: Deriving Schwa
+2.1 Voicing  
+2.2 Voicing Quanta  
+2.3 Vocal Tract Filtering  
+2.4 Pendulums, Standing Waves, and Vowel Formants  
+2.5 Discovering Nodes and Antinodes in an Acoustic Tube  
+
+#### Chapter 3. Digital Signal Processing... 49  
+3.1 Continuous versus Discrete Signals...  49    
+3.2 Analog-to-Digital Conversion... 50    
+3.2.1 Sampling ... 51   
+3.2.2 Quantization ... 55  
+3.3 Signal Analysis Methods ... 59  
+3.3.1 RMS amplitude ... 59  
+3.3.2 Fast Fourier transform (FFT) ... 60  
+3.3.3 Auto-correlation pitch tracking ... 64  
+3.3.4 Digital filters ... 68  
+3.3.5 Linear predictive coding (LPC) ... 71  
+3.3.6 Spectra and spectrograms ... 80  
+
+#### Chapter 4. Basic Audition ... 82    
+4.1 Anatomy of the Peripheral Auditory System ... 82  
+4.2 The Auditory Sensation of Loudness ... 83  
+4.3 Frequency Response of the Auditory System ... 88  
+4.4 Saturation and Masking ... 90  
+4.5 Auditory Representations ... 93  
+
+#### Chapter 5. Speech Perception ... 100  
+5.1 Auditory Ability Shapes Speech Perception ... 101  
+5.2 Phonetic Knowledge Shapes Speech Perception ... 104  
+5.2.1 Categorical perception ... 104  
+5.2.2 Phonetic coherence ... 109  
+5.3 Linguistic Knowledge Shapes Speech Perception ... 112  
+5.4 Perceptual Similarity ... 115  
+5.4.1 Maps from distances ... 116  
+5.4.2 The perceptual map of fricatives ... 119  
 """)
 
