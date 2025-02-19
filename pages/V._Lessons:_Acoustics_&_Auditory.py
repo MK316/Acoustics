87e16d0cacb88cdbfb5e6fdbda07a314ac151ef0
MK <99416359+MK316@users.noreply.github.com>
2025-02-19 17:28:21 +0900

    Update V._Lessons:_Acoustics_&_Auditory.py

--- pages/V._Lessons:_Acoustics_&_Auditory.py
+++ pages/V._Lessons:_Acoustics_&_Auditory.py
@@ -5,13 +5,13 @@ st.markdown("---")
 
 st.markdown("""
 #### Chapter 1. Basic Acoustics and Acoustic Filters
-1.1 The Sensation of Sound 7
-1.2 The Propagation of Sound 8
-1.3 Types of Sounds 11
-1.3.1 Simple periodic waves 11
-1.3.2 Complex periodic waves 12
-1.3.3 Aperiodic waves 17
-1.4 Acoustic Filters
+1.1 The Sensation of Sound  
+1.2 The Propagation of Sound  
+1.3 Types of Sounds  
+1.3.1 Simple periodic waves  
+1.3.2 Complex periodic waves  
+1.3.3 Aperiodic waves  
+1.4 Acoustic Filters  
 
 #### Chapter 2.
 #### Chapter 
