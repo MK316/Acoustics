472419f4a09ad77acbd4ce476c030763f87d09b9
MK <99416359+MK316@users.noreply.github.com>
2025-02-19 17:40:18 +0900

    Update V._Lessons:_Acoustics_&_Auditory.py

--- pages/V._Lessons:_Acoustics_&_Auditory.py
+++ pages/V._Lessons:_Acoustics_&_Auditory.py
@@ -8,11 +8,11 @@ st.markdown("""
 1.1 The Sensation of Sound  
 1.2 The Propagation of Sound  
 1.3 Types of Sounds  
-<ol>
+<ul>
 <li>1.3.1 Simple periodic waves</li>
-</ol>
-&nbsp;&nbsp;&nbsp;&nbsp;1.3.2 Complex periodic waves  
-&nbsp;&nbsp;&nbsp;&nbsp;1.3.3 Aperiodic waves  
+<li>1.3.2 Complex periodic waves</li>  
+<li>1.3.3 Aperiodic waves</li>
+</ul>
 1.4 Acoustic Filters  
 
 #### Chapter 2. The Acoustic Theory of Speech Production: Deriving Schwa
@@ -25,16 +25,19 @@ st.markdown("""
 #### Chapter 3. Digital Signal Processing... 49  
 3.1 Continuous versus Discrete Signals...  49    
 3.2 Analog-to-Digital Conversion... 50    
-&nbsp;&nbsp;&nbsp;&nbsp;3.2.1 Sampling ... 51   
-&nbsp;&nbsp;&nbsp;&nbsp;3.2.2 Quantization ... 55  
+<ul>
+<li>3.2.1 Sampling ... 51</li>   
+<li>3.2.2 Quantization ... 55</li>  
+</ul>
 3.3 Signal Analysis Methods ... 59  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.1 RMS amplitude ... 59  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.2 Fast Fourier transform (FFT) ... 60  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.3 Auto-correlation pitch tracking ... 64  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.4 Digital filters ... 68  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.5 Linear predictive coding (LPC) ... 71  
-&nbsp;&nbsp;&nbsp;&nbsp;3.3.6 Spectra and spectrograms ... 80  
-
+<ul>
+<li>3.3.1 RMS amplitude ... 59</li>  
+<li>3.3.2 Fast Fourier transform (FFT) ... 60</li>  
+<li>3.3.3 Auto-correlation pitch tracking ... 64</li>  
+<li>3.3.4 Digital filters ... 68 </li> 
+<li>3.3.5 Linear predictive coding (LPC) ... 71 </li> 
+<li>3.3.6 Spectra and spectrograms ... 80 </li> 
+</ul>
 #### Chapter 4. Basic Audition ... 82    
 4.1 Anatomy of the Peripheral Auditory System ... 82  
 4.2 The Auditory Sensation of Loudness ... 83  
@@ -45,11 +48,13 @@ st.markdown("""
 #### Chapter 5. Speech Perception ... 100  
 5.1 Auditory Ability Shapes Speech Perception ... 101  
 5.2 Phonetic Knowledge Shapes Speech Perception ... 104  
-5.2.1 Categorical perception ... 104  
-5.2.2 Phonetic coherence ... 109  
+<ul>
+<li>5.2.1 Categorical perception ... 104 </li> 
+<li>5.2.2 Phonetic coherence ... 109</li>   
 5.3 Linguistic Knowledge Shapes Speech Perception ... 112  
 5.4 Perceptual Similarity ... 115  
-5.4.1 Maps from distances ... 116  
-5.4.2 The perceptual map of fricatives ... 119  
+<li>5.4.1 Maps from distances ... 116</li>   
+<li>5.4.2 The perceptual map of fricatives ... 119</li>
+</ul>
 """, unsafe_allow_html=True)
 
