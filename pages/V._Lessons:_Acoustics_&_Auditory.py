947a213545147f14184b4416c79e5ba1099a1829
MK <99416359+MK316@users.noreply.github.com>
2025-02-19 17:27:56 +0900

    Update V._Lessons:_Acoustics_&_Auditory.py

--- pages/V._Lessons:_Acoustics_&_Auditory.py
+++ pages/V._Lessons:_Acoustics_&_Auditory.py
@@ -1,1 +1,21 @@
 import streamlit as st
+
+st.markdown("### Chapters")
+st.markdown("---")
+
+st.markdown("""
+#### Chapter 1. Basic Acoustics and Acoustic Filters
+1.1 The Sensation of Sound 7
+1.2 The Propagation of Sound 8
+1.3 Types of Sounds 11
+1.3.1 Simple periodic waves 11
+1.3.2 Complex periodic waves 12
+1.3.3 Aperiodic waves 17
+1.4 Acoustic Filters
+
+#### Chapter 2.
+#### Chapter 
+#### Chapter 
+#### Chapter 
+""")
+
