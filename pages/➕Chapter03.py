0866c67daed46b92f5dac42f4c079a9cc3b2549d
MK <99416359+MK316@users.noreply.github.com>
2025-02-20 14:59:40 +0900

    Update ➕Chapter03.py

--- pages/➕Chapter03.py
+++ pages/➕Chapter03.py
@@ -1,15 +1,32 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["📖 Lecture slides", "🌀 Apps", "💾 Download"])
+tab1, tab2, tab3 = st.tabs(["📖 Lecture slides", "Videolinks", "🌀 Apps", "💾 Download"])
 
-
+##########
 with tab1:
     st.write("Other content here.")
 
+##########
 with tab2:
-    st.caption("Apps to help the content")
-    
+    st.caption("Video contents")
+
+    video_url1 = "https://www.youtube.com/embed/hiflzRL7sUY?si=yI6gbYVtT99gEowg"
+
+    st.markdown("#### 1. Spectrogram generation demo (3D)")
+    st.markdown(
+        f"""
+        <iframe width="560" height="315" src="{video_url1}" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        """,
+        unsafe_allow_html=True
+    )
+##########
 with tab3:
+    st.caption("Apps to help the content")
+##########    
+with tab4:
     st.write("### Download Lecture Slides")
 
     # GitHub raw file URL (replace with your actual link)
