5acf28899512f2b90392c5f358a715e59007dba1
MK <99416359+MK316@users.noreply.github.com>
2025-02-21 12:16:24 +0900

    Update ➕Chapter04.py

--- pages/➕Chapter04.py
+++ pages/➕Chapter04.py
@@ -4,7 +4,7 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["📖 Lecture slides", "🌀 Web resourc
 
 
 with tab1:
-    st.write("Other content here.")
+    st.write("To be updated in time.")
 
 with tab2:
     st.write("1. The peripheral auditory system")
