51fea45df20ae7d1b175f9e7745784e32469de32
MK <99416359+MK316@users.noreply.github.com>
2025-02-25 17:40:35 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Main Page")
 
 # Add a button that links to another page inside the 'pages' folder
-st.page_link("praat/daga.py", label="Go to Page 1", icon="📄")
+st.page_link("pages/daga.py", label="Go to Page 1", icon="📄")
 st.page_link("pages/page2.py", label="Go to Page 2", icon="🔊")
 
