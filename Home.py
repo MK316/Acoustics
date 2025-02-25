4ea8037606865f83c57a6395502d9aaa2824a4f3
MK <99416359+MK316@users.noreply.github.com>
2025-02-25 14:44:39 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,14 +3,16 @@ import streamlit as st
 st.markdown("""
   #### 🐾 Phonology & English Education
   - Semester: Spring 2025
-  - Topic: Acoustics and Auditory Phonetics
+  - Topic: Acoustics and Auditory Phonetics (Physical aspect of sound)
+  - Questions:
+  1. What is sound, and how does it relate to teaching/learning spoken English?
+  2. How do English learners perceive sounds differently based on their linguistic background?
   """)
 
 st.image(
     "https://github.com/MK316/Acoustics/raw/main/images/acoustics-logo4.png",
     width=600,  # Adjust width as needed
-    caption= "Physical aspect of sound; What is sound, and how does it relate to teaching/learning spoken English?; How do English learners perceive sounds differently based on their linguistic background?"
-)
+    caption= "©️ MK316 Acoustics")
 
 st.markdown("---")
 st.markdown("⛺Goto [MK316 home](https://mk316.github.io)")
