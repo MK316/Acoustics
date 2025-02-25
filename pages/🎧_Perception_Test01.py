87919b684bc2056a6ad8fe70d4d05239c2854112
MK <99416359+MK316@users.noreply.github.com>
2025-02-25 18:06:41 +0900

    Update 🎧_Perception_Test01.py

--- pages/🎧_Perception_Test01.py
+++ pages/🎧_Perception_Test01.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Main Page")
 
 # Add a button that links to another page inside the 'pages' folder
-st.page_link("pages/daga.py", label="Go to Page 1", icon="📄")
+st.page_link("pages/daga.py", label="da-ga Perception", icon="📄")
