a18cdab13b9ab7579caccc3720167d8020c0aa9a
MK <99416359+MK316@users.noreply.github.com>
2025-03-05 21:49:26 +0900

    Delete pages/🎧_Perception_Test01.py

--- pages/🎧_Perception_Test01.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Simple perception tests")
-
-# Add a button that links to another page inside the 'pages' folder
-st.page_link("pages/daga.py", label="da-ga Perception", icon="🎧")
