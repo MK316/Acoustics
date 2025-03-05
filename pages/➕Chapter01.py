82a0d1cf19245c7f4d2fded93de93235c3a07757
MK <99416359+MK316@users.noreply.github.com>
2025-03-05 20:06:52 +0900

    Update ➕Chapter01.py

--- pages/➕Chapter01.py
+++ pages/➕Chapter01.py
@@ -6,6 +6,7 @@ import io
 import math
 import random
 import os
+from PIL import Image
 
 tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["📖 Lecture slides", "🌀 App1: Simple", "🌀App2: Complex", "🌀GCD", "💦 Quiz", "🌀 Videos", "💾 Download"])
 
