1250d7eee46fa0d5a1079ba3041a7180af73e278
MK <99416359+MK316@users.noreply.github.com>
2025-03-19 18:08:32 +0900

    Update ➕Chapter03.py

--- pages/➕Chapter03.py
+++ pages/➕Chapter03.py
@@ -16,10 +16,22 @@ with tab2:
 with tab3:
     st.caption("Video contents")
 
-    video_url1 = "https://www.youtube.com/embed/hiflzRL7sUY?si=yI6gbYVtT99gEowg"
-    video_url2 = "https://www.youtube.com/embed/-7PYX0ohST4?si=x_D5KnAd83NwSe1q"
+    video_url2 = "https://www.youtube.com/embed/hiflzRL7sUY?si=yI6gbYVtT99gEowg"
+    video_url1 = "https://www.youtube.com/embed/-7PYX0ohST4?si=x_D5KnAd83NwSe1q"
+
+    st.markdown("#### 1. How Does LP record music?")
+    st.markdown(
+        f"""
+        <iframe width="560" height="315" src="{video_url2}" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        """,
+        unsafe_allow_html=True
+    )
     
-    st.markdown("#### 1. Spectrogram generation demo (3D)")
+    
+    st.markdown("#### 2. Spectrogram generation demo (3D)")
     st.markdown(
         f"""
         <iframe width="560" height="315" src="{video_url1}" 
@@ -30,16 +42,7 @@ with tab3:
         unsafe_allow_html=True
     )
 
-    st.markdown("#### 2. How Does LP record music?")
-    st.markdown(
-        f"""
-        <iframe width="560" height="315" src="{video_url2}" 
-        title="YouTube video player" frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        """,
-        unsafe_allow_html=True
-    )
+
 ##########
 with tab4:
     st.caption("Apps to help the content")
